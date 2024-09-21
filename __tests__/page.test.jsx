87c0d3fe4dbe0e1948649a089a22eabd61cbe60a
Pontus Grandin <smeaurix@gmail.com>
2024-09-21 03:24:50 +0200

    resolve no message intl jest error

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -3,10 +3,17 @@ import { render, screen } from "@testing-library/react";
 import Home from "@/app/[locale]/page";
 import { NextIntlClientProvider } from "next-intl";
 
+const locale = "en";
+const messages = {
+  HomePage: {
+    welcome: "Welcome!",
+  },
+};
+
 describe("Page", () => {
   it("renders a heading", () => {
     render(
-      <NextIntlClientProvider locale="en">
+      <NextIntlClientProvider locale={locale} messages={messages}>
         <Home />
       </NextIntlClientProvider>,
     );
