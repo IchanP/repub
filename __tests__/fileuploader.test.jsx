ca6b49cfb1f0cad9217857849e2f4a69540b6e59
Pontus Grandin <smeaurix@gmail.com>
2024-10-11 07:43:16 +0200

    removal of errormessage test

--- __tests__/fileuploader.test.jsx
+++ __tests__/fileuploader.test.jsx
@@ -1,21 +1,41 @@
-import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, fireEvent, screen, cleanup } from "@testing-library/react";
 import FileUpload from "@/components/logic/FileUpload";
 
-describe("File Uploader", () => {
-  it("Renders error message on wrong file type", () => {
-    render(<FileUpload />);
-    // eslint-disable-next-line no-undef
-    const mockFile = new File(["invalid content"], "test.txt", {
-      type: "text/plain",
-    });
+// Helper function to render the component and upload a file
+const uploadMockFile = (fileName, fileType) => {
+  // eslint-disable-next-line no-undef
+  const mockFile = new File(["content"], fileName, { type: fileType });
 
-    const uploadButton = screen.getByTestId("file-upload");
-    fireEvent.change(uploadButton, { target: { files: [mockFile] } });
+  const uploadButton = screen.getByTestId("file-upload");
+  fireEvent.change(uploadButton, { target: { files: [mockFile] } });
+};
+
+afterEach(() => {
+  cleanup();
+});
+beforeEach(() => {
+  render(<FileUpload />);
+});
+
+describe("FileUpload Component", () => {
+  it("Renders error message on wrong file type", () => {
+    uploadMockFile("test.txt", "text/plain");
 
     const errorMessage = screen.getByText(
       "Only files in ePub format are allowed.",
     );
     expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("Removes the error message after valid ePub is selected", () => {
+    uploadMockFile("test.txt", "text/plain");
+    const errorMessage = screen.getByText(
+      "Only files in ePub format are allowed.",
+    );
+    expect(errorMessage).toBeInTheDocument();
+
+    uploadMockFile("valid.epub", "application/epub+zip");
+
+    expect(errorMessage).not.toBeInTheDocument();
   });
 });
--- src/components/logic/FileUpload.tsx
+++ src/components/logic/FileUpload.tsx
@@ -19,12 +19,15 @@ const FileUpload = () => {
 
   return (
     <div className="w-full">
-      <div className="absolute w-3/4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 dark:bg-darkSecondary p-2 rounded-md">
-        <label htmlFor="epub-file">
+      <div className="absolute w-3/4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 dark:bg-darkSecondary p-2 rounded-md md:w-2/4 md:text-center">
+        <label
+          htmlFor="epub-file"
+          className="md:flex md:flex-col md:items-center"
+        >
           <p className="text-center mb-1">Select book to upload</p>
           <input
             className="h-1/8 max-w-full cursor-pointer block text-sm text-gray-900 border bg-lightPrimary border-gray-300 rounded-lg cursor-pointer 
-          bg-gray-50 focus:outline-none dark:bg-darkPrimary dark:text-white dark:border-gray-600 dark:placeholder-gray-400 py-1 pl-1"
+          bg-gray-50 focus:outline-none dark:bg-darkPrimary dark:text-white dark:border-gray-600 dark:placeholder-gray-400 py-1 pl-1 md:w-3/4"
             id="epub-file"
             type="file"
             accept=".epub"
