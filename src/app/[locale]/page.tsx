1b76bc5b0a258caf2f198e76ff19332059384656
Pontus Grandin <smeaurix@gmail.com>
2024-10-11 03:41:34 +0200

    settings bar

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import ReaderWrapper from "@/components/logic/ReaderWrapper";
+import ReaderWrapper from "@/components/logic/ReaderLogic/ReaderWrapper";
 import ThemeSwitcher from "@/components/logic/ThemeSwitcher";
 /* import TestButton from "@/components/TestButton";
  */
@@ -6,11 +6,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="flex h-full w-full flex-col row-start-2 justify-center items-center">
+        <div className="bg-[#7b7481] h-full w-full">
+          {/*TODO the above div needs fixing in md+ */}
+          <ReaderWrapper />
+        </div>
         <div className="absolute left-0 top-0 hidden md:block">
           <ThemeSwitcher />
         </div>
-        <ReaderWrapper />
-        {/* <TestButton /> */}
       </main>
     </div>
   );
---
+++ src/components/logic/ReaderLogic/ReaderSettingsContainer.tsx
@@ -0,0 +1,6 @@
+// TODO in the future
+const ReaderSettingsContainer = () => {
+  return <div className="w-full dark:bg-darkPrimary">Test</div>;
+};
+
+export default ReaderSettingsContainer;
